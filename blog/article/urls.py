282b213640f095420b75e53381e3034409251fdd
aya520 <bayasayhi@gmail.com>
2018-01-10 13:40:21 +0800

    Chapter 10 finished

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^articleCreate/$', views.articleCreate, name='articleCreate'),
     url(r'^articleRead/(?P<articleId>[0-9]+)/$', views.articleRead, name='articleRead'),
     url(r'^articleUpdate/(?P<articleId>[0-9]+)/$', views.articleUpdate, name='articleUpdate'),
-    url(r'^articleUpdate/(?P<articleId>[0-9]+)/$', views.articleDelete, name='articleDelete'),
+    url(r'^articleDelete/(?P<articleId>[0-9]+)/$', views.articleDelete, name='articleDelete'),
     url(r'^articleSearch/$', views.articleSearch, name='articleSearch'),
 ]
